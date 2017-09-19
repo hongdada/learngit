8be1714836f7a65e74d11bd6a4d7c8fae491dbc6
hongdada <hongda159505@qq.com>
2017-09-19 20:25:15 +0800

    asdfas

--- HomeController.cs
+++ HomeController.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
-namespace lpapi.rongzi.com.Controllers
-{
-    public class HomeController : Controller
     {
         public ActionResult Index()
         {
