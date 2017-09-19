e2a2cbdf092076176059754a3b9a39f0e5879975
hongdada <hongda159505@qq.com>
2017-09-19 19:35:23 +0800

    提交Home

---
+++ HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lpapi.rongzi.com.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+    }
+}
+啊11111111111111111111111
